f606ec4ac0306e0289294e065f4e27cf2d84622d
HaseebACNCD <haseeb.ahmed@cncd.org>
2024-01-04 11:21:44 +0500

    aaj ka kaam

--- check.py
+++ check.py
@@ -6,12 +6,13 @@ print('haseeb')
 for i in range (10):
     print(i)
     print('usama')
-<<<<<<< HEAD
 print ('i am here ')
 print('ye kya chal raha haia')
-=======
 # print ('i am here ')
 
 
 print('aisa nahi chala ga main bhi check kar raha hun' )
->>>>>>> usama
+
+
+
+print('this is haseeb and i am working on it ')
